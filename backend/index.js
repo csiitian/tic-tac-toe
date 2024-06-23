63ddb7fd6dd3eff89898e080025079af69af3d90
Vikasss7663 <vishalsinghgk2018@gmail.com>
2024-06-23 14:33:18 +0530

    add cors for socket

--- backend/index.js
+++ backend/index.js
@@ -3,13 +3,22 @@ const app = express();
 const http = require('http');
 const server = http.createServer(app);
 const port = process.env.PORT || 3000;
+const cors = require('cors');
+
+// Allow all origins (use only for development)
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST'],
+  allowedHeaders: ['Content-Type'],
+  credentials: true,
+}));
 
 const initializeSocket = require('./app/socket/socketHandler');
 initializeSocket(server);
 
 app.get('/', (req, res) => {
   res.send('Welcome');
-})
+});
 
 server.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`);
